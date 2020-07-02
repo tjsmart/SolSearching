5d229ef96b047de2daaccb8d262ab503b335a567
Tyler Smart <tjsmart@ucsc.edu>
2020-07-01 23:12:21 -0700

    Found working version, will proceed with formerly known as 1.4_prop version

--- solar_tracker/solar_tracker.ino
+++ solar_tracker/solar_tracker.ino
@@ -1,43 +1,19 @@
 #include <VarSpeedServo.h>
-//SOME NOTES ABOUT VARSPEEDSERVO
-//attach(pin )  - Attaches a servo motor to an i/o pin.
-//attach(pin, min, max  ) - Attaches to a pin setting min and max values in microseconds
-//default min is 544, max is 2400  
 
-//write(value)     - Sets the servo angle in degrees.  (invalid angle that is valid as pulse in microseconds is treated as microseconds)
-//write(value, speed) - speed varies the speed of the move to new position 0=full speed, 1-255 slower to faster
-//write(value, speed, wait) - wait is a boolean that, if true, causes the function call to block until move is complete
-
-//writeMicroseconds() - Sets the servo pulse width in microseconds 
-//read()      - Gets the last written servo pulse width as an angle between 0 and 180. 
-//readMicroseconds()  - Gets the last written servo pulse width in microseconds. (was read_us() in first release)
-//attached()  - Returns true if there is a servo attached. 
-//detach()    - Stops an attached servos from pulsing its i/o pin. 
-
-//slowmove(value, speed) - The same as write(value, speed), retained for compatibility with Korman's version
-
-//stop() - stops the servo at the current position
-
-//sequencePlay(sequence, sequencePositions); // play a looping sequence starting at position 0
-//sequencePlay(sequence, sequencePositions, loop, startPosition); // play sequence with number of positions, loop if true, start at position
-//sequenceStop(); // stop sequence at current position
-//wait(); // wait for movement to finish
-//isMoving()  // return true if servo is still moving
-
-
-
-VarSpeedServo hserv;          // create servo object to control a servo
+// instatiate two servos
+// for more info see https://github.com/netlabtoolkit/VarSpeedServo/blob/master/VarSpeedServo.cpp
+VarSpeedServo hserv;
 VarSpeedServo vserv;         
 
-int sensorTwo = 2;       //digital pin 2 connected to the resistor of first sensor
+int sensorTwo = 5;       //digital pin 2 connected to the resistor of first sensor
 int sensorThree = 3;       //
-int sensorFour = 4;
-int sensorFive = 5;
+int sensorFour = 2;
+int sensorFive = 4;
 
-int inputTwo = 2;        //variable to store the values from the analog pin 2
+int inputTwo = 5;        //variable to store the values from the analog pin 2
 int inputThree = 3;       
-int inputFour = 4;        
-int inputFive = 5;        
+int inputFour = 2;        
+int inputFive = 4;        
 
 int hpos = 85;        // variable to store the servo's horizontal position
 int vpos = 62;        
